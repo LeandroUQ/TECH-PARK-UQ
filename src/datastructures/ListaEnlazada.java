b06cfbabf75d05fda0ea9a79954912f16f4e50d0
Mateo <mateogarridorios@gmail.com>
2026-05-12 22:19:04 -0500

    feat: Lógica clase ListaEnlazada

--- src/datastructures/ListaEnlazada.java
+++ src/datastructures/ListaEnlazada.java
@@ -1,4 +1,94 @@
 package datastructures;
 
-public class ListaEnlazada {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ListaEnlazada<T> implements Iterable<T> {
+
+    private static class Nodo<T> {
+        T dato;
+        Nodo<T> siguiente;
+
+        Nodo(T dato) {
+            this.dato = dato;
+        }
+    }
+
+    private Nodo<T> cabeza;
+    private int tamano;
+
+    public void agregar(T dato) {
+        Nodo<T> nuevo = new Nodo<>(dato);
+        if (cabeza == null) {
+            cabeza = nuevo;
+        } else {
+            Nodo<T> actual = cabeza;
+            while (actual.siguiente != null) actual = actual.siguiente;
+            actual.siguiente = nuevo;
+        }
+        tamano++;
+    }
+
+    public void agregarAlInicio(T dato) {
+        Nodo<T> nuevo = new Nodo<>(dato);
+        nuevo.siguiente = cabeza;
+        cabeza = nuevo;
+        tamano++;
+    }
+
+    public T obtener(int indice) {
+        validarIndice(indice);
+        Nodo<T> actual = cabeza;
+        for (int i = 0; i < indice; i++) actual = actual.siguiente;
+        return actual.dato;
+    }
+
+    public boolean eliminar(T dato) {
+        if (cabeza == null) return false;
+        if (cabeza.dato.equals(dato)) {
+            cabeza = cabeza.siguiente;
+            tamano--;
+            return true;
+        }
+        Nodo<T> actual = cabeza;
+        while (actual.siguiente != null) {
+            if (actual.siguiente.dato.equals(dato)) {
+                actual.siguiente = actual.siguiente.siguiente;
+                tamano--;
+                return true;
+            }
+            actual = actual.siguiente;
+        }
+        return false;
+    }
+
+    public boolean contiene(T dato) {
+        for (T item : this) if (item.equals(dato)) return true;
+        return false;
+    }
+
+    public int tamano() { return tamano; }
+    public boolean estaVacia() { return tamano == 0; }
+
+    private void validarIndice(int indice) {
+        if (indice < 0 || indice >= tamano)
+            throw new IndexOutOfBoundsException("Indice: " + indice + ", Tamano: " + tamano);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            Nodo<T> actual = cabeza;
+
+            @Override public boolean hasNext() { return actual != null; }
+
+            @Override
+            public T next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                T dato = actual.dato;
+                actual = actual.siguiente;
+                return dato;
+            }
+        };
+    }
 }
