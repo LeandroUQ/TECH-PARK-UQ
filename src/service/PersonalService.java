f4fc19cc0ff09f2d04f8e0f0a537f99b048a6425
Leandro337 <oscaragudelofranco@gmail.com>
2026-05-15 10:18:12 -0500

    Lógica PersonalService
    Implementación de la lógica de la clase PersonalService

--- src/service/PersonalService.java
+++ src/service/PersonalService.java
@@ -1,4 +1,46 @@
 package service;
 
+import dao.PersonalDAO;
+import datastructures.ListaEnlazada;
+import model.Administrador;
+import model.Operador;
+import model.Personal;
+
 public class PersonalService {
+
+    private static PersonalService instancia;
+    private final PersonalDAO dao = PersonalDAO.getInstance();
+
+    private PersonalService() {}
+
+    public static PersonalService getInstance() {
+        if (instancia == null) instancia = new PersonalService();
+        return instancia;
+    }
+
+    public void registrar(Personal p) {
+        if (dao.buscarPorId(p.getId()) != null)
+            throw new IllegalArgumentException("Ya existe un empleado con id: " + p.getId());
+        dao.agregar(p);
+    }
+
+    public Personal buscarPorId(String id) {
+        return dao.buscarPorId(id);
+    }
+
+    public ListaEnlazada<Personal> listarTodos() {
+        return dao.listarTodos();
+    }
+
+    public ListaEnlazada<Operador> listarOperadores() {
+        return dao.listarOperadores();
+    }
+
+    public ListaEnlazada<Administrador> listarAdministradores() {
+        return dao.listarAdministradores();
+    }
+
+    public boolean eliminar(String id) {
+        return dao.eliminar(id);
+    }
 }
